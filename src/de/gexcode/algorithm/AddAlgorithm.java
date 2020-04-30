f118dc63cd3009d1342fd69b90f866582af3d37d
GexCoder20 <georg.orfali20@gmail.com>
2020-04-30 23:21:07 +0200

    Fixing Bug

--- src/de/gexcode/algorithm/AddAlgorithm.java
+++ src/de/gexcode/algorithm/AddAlgorithm.java
@@ -8,7 +8,6 @@ public class AddAlgorithm {
 		JVArray data = new JVArray(100);
 		data.generateValue(10, 100);
 		new BubbleSort("BubbleSort", data);
-		new BubbleSort("BubbleSort2", data);
 	}
 
 }
